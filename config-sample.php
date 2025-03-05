e410a511c21d469d1e3d64525d2a0be5149ed71f
root <root@vps-280a6f6f.vps.ovh.net>
2025-03-05 12:06:29 +0000

    Ajout config-sample

---
+++ config-sample.php
@@ -0,0 +1,55 @@
+<?php
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Online Appointment Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) Alex Tselegidis
+ * @license     https://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        https://easyappointments.org
+ * @since       v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+/**
+ * Easy!Appointments Configuration File
+ *
+ * Set your installation BASE_URL * without the trailing slash * and the database
+ * credentials in order to connect to the database. You can enable the DEBUG_MODE
+ * while developing the application.
+ *
+ * Set the default language by changing the LANGUAGE constant. For a full list of
+ * available languages look at the /application/config/config.php file.
+ *
+ * IMPORTANT:
+ * If you are updating from version 1.0 you will have to create a new "config.php"
+ * file because the old "configuration.php" is not used anymore.
+ */
+class Config
+{
+    // ------------------------------------------------------------------------
+    // GENERAL SETTINGS
+    // ------------------------------------------------------------------------
+
+    const BASE_URL = 'http://test.dnsinfo.fr/easyappointments/';
+    const LANGUAGE = 'english';
+    const DEBUG_MODE = true;
+
+    // ------------------------------------------------------------------------
+    // DATABASE SETTINGS
+    // ------------------------------------------------------------------------
+
+    const DB_HOST = '';
+    const DB_NAME = '';
+    const DB_USERNAME = '';
+    const DB_PASSWORD = '';
+
+    // ------------------------------------------------------------------------
+    // GOOGLE CALENDAR SYNC
+    // ------------------------------------------------------------------------
+
+    const GOOGLE_SYNC_FEATURE = FALSE; // Enter TRUE or FALSE
+    const GOOGLE_CLIENT_ID = '';
+    const GOOGLE_CLIENT_SECRET = '';
+    const GOOGLE_API_KEY = '';
+
+}
